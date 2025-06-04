d772ae3069c4ac5cea4b42ff1e5767c087279fc0
GitHub Action <action@github.com>
2025-06-04 03:49:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/tmcmillin24/myhelloplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/tmcmillin24/myhelloplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.tmcmillin24.myhelloplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
